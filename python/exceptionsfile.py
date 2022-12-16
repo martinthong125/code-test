2adf4410e5ff1030fcc6ba79c08b7f94c5d89648
Martin Thong <martinthong125@gmail.com>
2022-12-16 15:20:34 +0800

    modified:   python/exceptionsfile.py Changed to a better method to handle exception by printing default error message and adding hints based on experience.

--- python/exceptionsfile.py
+++ python/exceptionsfile.py
@@ -9,8 +9,9 @@ import sys
 # try:
 #     print(4 / 0)
 
-# except ZeroDivisionError:
-#     print("divide by 0 is not possible.")
+# except ZeroDivisionError as e:
+#     print(e)
+#     print("Hint: You should not divide by 0.")
 # except Exception as e:
 #     print(e)
 #     # print("zero division error.")
@@ -35,22 +36,29 @@ try:
     # print(4 + "abc")
     # print(4 / 0)
     # import bash_scripting
+
     pass
 
-except FileNotFoundError:
-    print("File is not present in path.")
+except FileNotFoundError as e:
+    print(e)
+    print("Hint: Check that the file is present in given path.")
     sys.exit(1)
-except NameError:
-    print("Variable is not defined.")
+except NameError as e:
+    print(e)
+    print("Hint: Define your variable before use.")
     sys.exit(2)
-except TypeError:
-    print("Operation on different datatype is not possible.")
+except TypeError as e:
+    print(e)
+    print("Hint: Change your datatype to int or float.")
     sys.exit(3)
-except ZeroDivisionError:
-    print("Division by zero is not possible.")
+except ZeroDivisionError as e:
+    print(e)
+    print("Hint: Did you intend to divide by 10 by key in 0 only?")
     sys.exit(4)
-except ModuleNotFoundError:
-    print("Please install the module before importing.")
+except ModuleNotFoundError as e:
+    print(e)
+    print("Hint1: Please install the module before importing.")
+    print("Hint2: Please ensure that the module exists before importing.")
     sys.exit(5)
 except Exception as e:
     print(e)
@@ -60,15 +68,15 @@ finally:
     print("Finally, this will execute whether pass or fail.")
 
 # try except else block
-try:
-    a = 10
-    print(a)
-except Exception as e:
-    print(e)
-else:
-    print("Else: This will execute if there is no exception in try block")
-finally:
-    print("Finally:  This will always be printed.")
+# try:
+#     a = 10
+#     print(a)
+# except Exception as e:
+#     print(e)
+# else:
+#     print("Else: This will execute if there is no exception in try block")
+# finally:
+#     print("Finally:  This will always be printed.")
 
 
 print("This line will always be printed.")
